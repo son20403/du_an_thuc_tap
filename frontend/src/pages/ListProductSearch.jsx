75af671d964b0151f181a3e616e864d1743d8450
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-11-17 22:50:56 +0700

    feature/search

--- frontend/src/pages/ListProductSearch.jsx
+++ frontend/src/pages/ListProductSearch.jsx
@@ -1,11 +1,318 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import products from '../data/products';
+import { Link, useLocation } from 'react-router-dom';
+import Instagram from '../layouts/Instagram';
+import $ from 'jquery'
+import mixitup from 'mixitup'
+import useCurrencyFormat from '../hooks/useCurrencyFormat';
 const ListPostSearch = () => {
+    const [dataProducts, setDataProducts] = useState([]);
+    const [query, setQuery] = useState('');
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search).get("query");
+    const listProductsSearch = dataProducts?.filter(
+        pro => pro.slug.toLowerCase().includes(query.toLowerCase())
+            || pro.ten_sp.toLowerCase().includes(query.toLowerCase()))
+    useEffect(() => {
+        setDataProducts(products)
+        setQuery(searchParams)
+    }, [products, searchParams]);
+
+    useEffect(() => {
+        $('.filter__controls li').on('click', function () {
+            $('.filter__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.property__gallery').length > 0) {
+            var containerEl = document.querySelector('.property__gallery');
+            var mixer = mixitup(containerEl);
+        }
+        //Canvas Menu
+        $(".canvas__open").on('click', function () {
+            $(".offcanvas-menu-wrapper").addClass("active");
+            $(".offcanvas-menu-overlay").addClass("active");
+        });
+
+        $(".offcanvas-menu-overlay, .offcanvas__close").on('click', function () {
+            $(".offcanvas-menu-wrapper").removeClass("active");
+            $(".offcanvas-menu-overlay").removeClass("active");
+        });
+        $('.image-popup').magnificPopup({
+            type: 'image'
+        });
+        $(".nice-scroll").niceScroll({
+            cursorborder: "",
+            cursorcolor: "#dddddd",
+            boxzoom: false,
+            cursorwidth: 5,
+            background: 'rgba(0, 0, 0, 0.2)',
+            cursorborderradius: 50,
+            horizrailenabled: false
+        });
+    }, [listProductsSearch]);
+    useEffect(() => {
+        const setBgElements = document.querySelectorAll('.set-bg');
+        setBgElements.forEach(element => {
+            const bg = element.getAttribute('data-setbg');
+            element.style.backgroundImage = `url(${bg})`;
+        });
+    }, [listProductsSearch]);
     return (
         <div>
-
+            <section className="shop spad">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-3">
+                            <div className="shop__sidebar">
+                                <div className="sidebar__categories">
+                                    <div className="section-title">
+                                        <h4>Categories</h4>
+                                    </div>
+                                    <div className="categories__accordion">
+                                        <div className="accordion" id="accordionExample">
+                                            <div className="card">
+                                                <div className="card-heading active">
+                                                    <a data-toggle="collapse" data-target="#collapseOne">Women</a>
+                                                </div>
+                                                <div id="collapseOne" className="collapse show" data-parent="#accordionExample">
+                                                    <div className="card-body">
+                                                        <ul>
+                                                            <li><a href="#">Coats</a></li>
+                                                            <li><a href="#">Jackets</a></li>
+                                                            <li><a href="#">Dresses</a></li>
+                                                            <li><a href="#">Shirts</a></li>
+                                                            <li><a href="#">T-shirts</a></li>
+                                                            <li><a href="#">Jeans</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="card">
+                                                <div className="card-heading">
+                                                    <a data-toggle="collapse" data-target="#collapseTwo">Men</a>
+                                                </div>
+                                                <div id="collapseTwo" className="collapse" data-parent="#accordionExample">
+                                                    <div className="card-body">
+                                                        <ul>
+                                                            <li><a href="#">Coats</a></li>
+                                                            <li><a href="#">Jackets</a></li>
+                                                            <li><a href="#">Dresses</a></li>
+                                                            <li><a href="#">Shirts</a></li>
+                                                            <li><a href="#">T-shirts</a></li>
+                                                            <li><a href="#">Jeans</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="card">
+                                                <div className="card-heading">
+                                                    <a data-toggle="collapse" data-target="#collapseThree">Kids</a>
+                                                </div>
+                                                <div id="collapseThree" className="collapse" data-parent="#accordionExample">
+                                                    <div className="card-body">
+                                                        <ul>
+                                                            <li><a href="#">Coats</a></li>
+                                                            <li><a href="#">Jackets</a></li>
+                                                            <li><a href="#">Dresses</a></li>
+                                                            <li><a href="#">Shirts</a></li>
+                                                            <li><a href="#">T-shirts</a></li>
+                                                            <li><a href="#">Jeans</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="card">
+                                                <div className="card-heading">
+                                                    <a data-toggle="collapse" data-target="#collapseFour">Accessories</a>
+                                                </div>
+                                                <div id="collapseFour" className="collapse" data-parent="#accordionExample">
+                                                    <div className="card-body">
+                                                        <ul>
+                                                            <li><a href="#">Coats</a></li>
+                                                            <li><a href="#">Jackets</a></li>
+                                                            <li><a href="#">Dresses</a></li>
+                                                            <li><a href="#">Shirts</a></li>
+                                                            <li><a href="#">T-shirts</a></li>
+                                                            <li><a href="#">Jeans</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="card">
+                                                <div className="card-heading">
+                                                    <a data-toggle="collapse" data-target="#collapseFive">Cosmetic</a>
+                                                </div>
+                                                <div id="collapseFive" className="collapse" data-parent="#accordionExample">
+                                                    <div className="card-body">
+                                                        <ul>
+                                                            <li><a href="#">Coats</a></li>
+                                                            <li><a href="#">Jackets</a></li>
+                                                            <li><a href="#">Dresses</a></li>
+                                                            <li><a href="#">Shirts</a></li>
+                                                            <li><a href="#">T-shirts</a></li>
+                                                            <li><a href="#">Jeans</a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="sidebar__filter">
+                                    <div className="section-title">
+                                        <h4>Shop by price</h4>
+                                    </div>
+                                    <div className="filter-range-wrap">
+                                        <div className="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content" data-min={33} data-max={99} />
+                                        <div className="range-slider">
+                                            <div className="price-input">
+                                                <p>Price:</p>
+                                                <input type="text" id="minamount" />
+                                                <input type="text" id="maxamount" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="#">Filter</a>
+                                </div>
+                                <div className="sidebar__sizes">
+                                    <div className="section-title">
+                                        <h4>Shop by size</h4>
+                                    </div>
+                                    <div className="size__list">
+                                        <label htmlFor="xxs">
+                                            xxs
+                                            <input type="checkbox" id="xxs" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="xs">
+                                            xs
+                                            <input type="checkbox" id="xs" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="xss">
+                                            xs-s
+                                            <input type="checkbox" id="xss" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="s">
+                                            s
+                                            <input type="checkbox" id="s" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="m">
+                                            m
+                                            <input type="checkbox" id="m" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="ml">
+                                            m-l
+                                            <input type="checkbox" id="ml" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="l">
+                                            l
+                                            <input type="checkbox" id="l" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="xl">
+                                            xl
+                                            <input type="checkbox" id="xl" />
+                                            <span className="checkmark" />
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="sidebar__color">
+                                    <div className="section-title">
+                                        <h4>Shop by size</h4>
+                                    </div>
+                                    <div className="size__list color__list">
+                                        <label htmlFor="black">
+                                            Blacks
+                                            <input type="checkbox" id="black" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="whites">
+                                            Whites
+                                            <input type="checkbox" id="whites" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="reds">
+                                            Reds
+                                            <input type="checkbox" id="reds" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="greys">
+                                            Greys
+                                            <input type="checkbox" id="greys" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="blues">
+                                            Blues
+                                            <input type="checkbox" id="blues" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="beige">
+                                            Beige Tones
+                                            <input type="checkbox" id="beige" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="greens">
+                                            Greens
+                                            <input type="checkbox" id="greens" />
+                                            <span className="checkmark" />
+                                        </label>
+                                        <label htmlFor="yellows">
+                                            Yellows
+                                            <input type="checkbox" id="yellows" />
+                                            <span className="checkmark" />
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-9 col-md-9">
+                            <div className="row">
+                                {listProductsSearch && listProductsSearch.length > 0
+                                    ? listProductsSearch.map((product) => (
+                                        <ProductItem key={product.id} product={product}></ProductItem>
+                                    )) : ('Không có sản phẩm')}
+                                <div className="col-lg-12 text-center">
+                                    <div className="pagination__option">
+                                        <a href="#">1</a>
+                                        <a href="#">2</a>
+                                        <a href="#">3</a>
+                                        <a href="#"><i className="fa fa-angle-right" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Instagram></Instagram>
         </div>
     );
 };
+const ProductItem = ({ product }) => {
+    const formattedAmount = useCurrencyFormat(product.gia);
+    return (
+        <div className="col-lg-4 col-md-6">
+            <div className="product__item">
+                <div className="product__item__pic set-bg" data-setbg={product.anh_sp}>
+                    <div className="label new">New</div>
+                    <ul className="product__hover">
+                        <li><a href={product.anh_sp} className="image-popup">
+                            <span className="arrow_expand" /></a></li>
+                        <li><Link to={`/detail/${product.slug}`}><span className="icon_bag_alt" /></Link></li>
+                    </ul>
+                </div>
+                <div className="product__item__text">
+                    <h6><a href="#">{product.ten_sp}</a></h6>
+                    <div className="product__price">{formattedAmount}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default ListPostSearch;
