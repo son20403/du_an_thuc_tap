9c392c329271afb7dc578e8ba53606d38f7ea2c3
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-11-30 10:57:00 +0700

    update them gio hang

--- frontend/src/layouts/CategoryHomeItem.jsx
+++ frontend/src/layouts/CategoryHomeItem.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import products from '../data/products';
+import useGetAllProducts from '../hooks/useGetAllPost';
 
 const CategoryHomeItem = ({ cate, index }) => {
-    const [dataProducts, setDataProducts] = useState([]);
-    const totalProducts = dataProducts?.filter((pro) => pro.ma_loai === cate?.id).length
-    useEffect(() => {
-        setDataProducts(products)
-    }, [products]);
+    const { dataProducts } = useGetAllProducts()
+    const totalProducts = dataProducts?.filter((pro) => pro.ma_the_loai === cate?.id).length
     return (
         <div key={cate.id} className="col-lg-6 col-md-6 col-sm-6 p-0">
             <div className="categories__item set-bg" data-setbg={`./src/assets/img/categories/category-${index + 2}.jpg`}>
                 <div className="categories__text">
-                    <h4>{cate.ten_danh_muc}</h4>
+                    <h4>{cate.ten_the_loai}</h4>
                     <p>{totalProducts} sản phẩm</p>
-                    <Link to={`/category/${cate.slug}`}>Xem ngay</Link>
+                    <Link to={`/category/${cate.ten_the_loai_slug}`}>Xem ngay</Link>
                 </div>
             </div>
         </div>
