9dec1b3bccd90e3dee7d1f695229f08dc740b99d
Anh Tu <119322350+nguyenanhtu2003@users.noreply.github.com>
2023-11-30 19:01:40 +0700

    Add files via upload

--- laravel-react/routes/api.php
+++ laravel-react/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+Route::post('/register', [RegisterController::class, 'methodName']);
+
+Route::post('/login', [AuthController::class, 'login']);
