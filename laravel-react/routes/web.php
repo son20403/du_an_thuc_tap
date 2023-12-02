51cd4e63b4c8cd7d50bf86f5bd534249d023bdce
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-12-02 08:23:03 +0700

    update serrver

--- laravel-react/routes/web.php
+++ laravel-react/routes/web.php
@@ -31,7 +31,7 @@ Route::middleware([
         Route::get('/', function () {
             return view('admin.page.home.index');
         }); // url/admin
-    
+
         Route::group(['prefix' => '/danh-muc'], function () {
             Route::get('/', [DanhMucController::class,'index']);
             Route::get('/du-lieu', [DanhMucController::class,'HienThiDanhMuc']); // url/admin/danh-muc/du-lieu
@@ -39,7 +39,7 @@ Route::middleware([
             Route::post('/xoa', [DanhMucController::class,'XoaDanhMuc']);
             Route::post('/cap-nhat', [DanhMucController::class,'CapNhatDanhMuc']);
         });
-    
+
         Route::group(['prefix' => '/the-loai'], function () {
             Route::get('/', [TheLoaiController::class,'index']);
             Route::get('/du-lieu', [TheLoaiController::class,'HienThiTheLoai']); // url/admin/the-loa/du-lieu
@@ -47,7 +47,7 @@ Route::middleware([
             Route::post('/xoa', [TheLoaiController::class,'XoaTheLoai']);
             Route::post('/cap-nhat', [TheLoaiController::class,'CapNhatTheLoai']);
         });
-    
+
         Route::group(['prefix' => '/san-pham'], function () {
             // Route::get('/', [SanPhamController::class,'index']);
             // Route::get('/du-lieu', [SanPhamController::class,'HienThiSanPham']); // url/admin/the-loa/du-lieu
@@ -55,17 +55,17 @@ Route::middleware([
             // Route::post('/xoa', [SanPhamController::class,'XoaSanPham']);
             // Route::post('/cap-nhat', [SanPhamController::class,'CapNhatSanPham']);
             // Route::post('/toggleStatus/{id}', [SanPhamController::class,'CapNhatSanPham']);
-    
+
             Route::get('/', [SanphamController::class, 'sanpham']);
             Route::get('/du-lieu', [SanPhamController::class,'HienThiSanPham']); // url/admin/the-loa/du-lieu
             Route::get('/xoasanpham', [SanphamController::class, 'xoa_sanpham']);
             Route::post('/capnhatsanpham/{id}', [SanphamController::class, 'cn_sanpham_']);
             Route::get('/toggleStatus', [SanphamController::class, 'toggleStatus']);
         });
-    
+
         // Hình Ảnh
         Route::get('/xoahinhanh', [HinhAnhController::class, 'xoa_hinhanh']);
-    
+
         Route::group(['prefix' => '/gio-hang'], function () {
             Route::get('/', [GioHangController::class,'index']);
             Route::get('/du-lieu', [GioHangController::class,'HienThiGioHang']); // url/admin/the-loa/du-lieu
@@ -77,9 +77,38 @@ Route::middleware([
 });
 
 
+Route::group(['prefix'=> 'admin'], function () {
+    Route::get('/', function () {
+        return view('admin.page.home.index');
+    }); // url/admin
 
+    Route::group(['prefix' => '/danh-muc'], function () {
+        Route::get('/du-lieu', [DanhMucController::class,'HienThiDanhMuc']); // url/admin/danh-muc/du-lieu
+    });
 
-    
+    Route::group(['prefix' => '/the-loai'], function () {
+        Route::get('/du-lieu', [TheLoaiController::class,'HienThiTheLoai']); // url/admin/the-loa/du-lieu
+    });
+
+    Route::group(['prefix' => '/san-pham'], function () {
+        // Route::get('/', [SanPhamController::class,'index']);
+        // Route::get('/du-lieu', [SanPhamController::class,'HienThiSanPham']); // url/admin/the-loa/du-lieu
+        // Route::post('/', [SanPhamController::class,'ThemSanPham']);
+        // Route::post('/xoa', [SanPhamController::class,'XoaSanPham']);
+        // Route::post('/cap-nhat', [SanPhamController::class,'CapNhatSanPham']);
+        // Route::post('/toggleStatus/{id}', [SanPhamController::class,'CapNhatSanPham']);
+
+        Route::get('/du-lieu', [SanPhamController::class,'HienThiSanPham']); // url/admin/the-loa/du-lieu
+    });
+
+    // Hình Ảnh
+    Route::get('/xoahinhanh', [HinhAnhController::class, 'xoa_hinhanh']);
+
+    Route::group(['prefix' => '/gio-hang'], function () {
+        Route::get('/du-lieu', [GioHangController::class,'HienThiGioHang']); // url/admin/the-loa/du-lieu
+    });
+});// admin
+
 
 Route::post('/send-email', [MailController::class, 'sendEmail']);
 
