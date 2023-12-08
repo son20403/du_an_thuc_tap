be6975e238140b9073f4f7a0f364664c22d5dbcb
dongtam <ldt21012017@gmail.com>
2023-12-08 21:24:19 +0700

    checkout

--- laravel-react/routes/api.php
+++ laravel-react/routes/api.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\AuthController;
+use App\Actions\Fortify\CreateNewUser;
 use App\Http\Controllers\CheckoutController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,5 +20,30 @@ use App\Http\Controllers\CheckoutController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+
+
+// Route::post('/register', [RegisterController::class, 'methodName']);
+
+// Route::post('/login', [AuthController::class, 'login']);
+
+
+// register
+Route::post('/register', [UserController::class, 'store']);
+// update user
+Route::post('/update-user', [UserController::class, 'update']);
+// login nhận dữ liệu user
+Route::post('/login', [UserController::class, 'login']);
+// login nhận toker
+Route::post('/login-token', [UserController::class, 'login_token']);
+// detail-user để lấy dữ liệu user
+Route::post('/detail-user', [UserController::class, 'detailUser'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/logout-token', [UserController::class, 'logout_token']);
+});
+
+//checkout
 Route::post('/api/checkout', [CheckoutController::class, 'processCheckout']);
